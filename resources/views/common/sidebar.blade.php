d0f77f95675f11084b7b9d9389f4cce73e182278
joshuacjj7 <111513692+joshuacjj7@users.noreply.github.com>
2023-11-05 20:25:58 +0800

    Update sidebar.blade.php

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -182,33 +182,6 @@
         <hr class="sidebar-divider d-none d-md-block">
     @endif
 
-    @if(auth()->user()->hasRole('SuperAdmin'))
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Admin Section
-    </div>
-
-   <!-- Nav Item - Pages Collapse Menu -->
-   <li class="nav-item">
-       <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-           aria-expanded="true" aria-controls="collapsePages">
-           <i class="fas fa-fw fa-folder"></i>
-           <span>Roles & Permissions</span>
-       </a>
-       <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-           <div class="bg-white py-2 collapse-inner rounded">
-               <h6 class="collapse-header">Roles & Permissions</h6>
-               <a class="collapse-item" href="{{ route('roles.index') }}">Roles</a>
-               <a class="collapse-item" href="{{ route('permissions.index') }}">Permissions</a>
-           </div>
-       </div>
-   </li>
-
-   <!-- Divider -->
-   <hr class="sidebar-divider d-none d-md-block">
-@endif
-
-
     <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
