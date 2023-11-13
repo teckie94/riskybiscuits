fd95681359f8b9fe9d47a1f3fd6e059f7c92a906
racezx812 <59534186+racezx812@users.noreply.github.com>
2023-11-13 18:14:22 +0800

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,17 +5,17 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+class UserTest extends TestCase
+{
+    public function test_login_redirect_to_dashboard_successfully() {
+
+       $response = $this->post('/login',[
+        'email'=> 'superadmin@gmail.com',
+        'password' => '111'
+       ]);
+
+       $response->assertStatus(302);
+       $response->assertRedirect('/home');
     }
 }
