b2e6c99b8d79b07055e57cc84c33d9d2e0ef6532
racezx812 <59534186+racezx812@users.noreply.github.com>
2023-11-13 19:32:51 +0800

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,17 +3,26 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use DatabaseMigrations;
-
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserTest extends TestCase
 {
     public function test_login_redirect_to_dashboard_successfully() {
+       User::factory()->create([
+        'first_name'    => 'Super',
+        'last_name'     => 'Admin2',
+        'email'         =>  'superadmin2@gmail.com',
+        'mobile_number' =>  '81818181',
+        'password'      =>  Hash::make('password'),
+        'role_id'       => 1
+       ]);
 
        $response = $this->post('/login',[
-        'email'=> 'superadmin@gmail.com',
-        'password' => '111'
+        'email'=> 'superadmin2@gmail.com',
+        'password' => 'password'
        ]);
 
        $response->assertStatus(302);
